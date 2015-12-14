89bcc33b1cdb718e2bda53bf711beeb7db00e5db
kelvinhado <kelvinhado@outlook.com>
2015-12-14 18:33:10 +0100

    V2, change server architecture : now using rooter / clean scrapper : removed local registration, add db hundle / add model to use mongo db

--- remixjob-scrapper.js
+++ remixjob-scrapper.js
@@ -4,38 +4,28 @@ var   fs = require('fs'),
       async = require('async'),
       Job = require('./remixjob-model');
 
-/** parameters
-  * @nbPages : number or pages to scrapp
-  * return : number of jobs scrapped
-*/
+
 var remixUrl = "https://remixjobs.com";
 
 exports.scrappRemixJob = function(nbPages, exportCallback) {
-
-  var hits = 0;
-  var data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
 
   // this function will run n times the same code and stop when every callback have been called.
   async.times(nbPages, function(n, next){
       var url = generateUrl(n+1);
       var req = request(url, function(error, response, html){
-            var jsonResult;
+            var local_hits = 0;
              if(!error){
-                jsonResult = scrappHtml(html);
+                scrappHtml(html);
              }//end if request error
-             next(error, jsonResult);
+             next(error, local_hits);
       }); // end request
-  }, function(error, jsonResults) { //jsonResults (with s) contain all the results
+  }, function(error, local_hits_table) { //jsonResults (with s) contain all the results
 
-    // merging results
-    // we do have n table of results inside jsonResults, let's merge them all
-      for(var i=1; i<jsonResults.length; i++) {
-        jsonResults[0] = jsonResults[0].concat(jsonResults[i]);
+      var total_hits = 0;
+      for(var i=0; i<total_hits.length; i++) {
+        total_hits += local_hits_table[i];
       }
-
-       data.records = jsonResults[0];
-       fs.writeFileSync('./data.json', JSON.stringify(data));
-       exportCallback(data.records.length);
+       exportCallback(total_hits);
     });
 
 }; // end exports.scrappRemixJob
@@ -45,30 +35,14 @@ function generateUrl(page) {
   return remixUrl + "/?page=" + page + "&in=all";
 }
 
+
 /**
- * @return jobResults in Json format
+ *  SCRAP adn SAVE jobs into the db
  */
 function scrappHtml(html){
-
-       var jsonResult = [];
        var $ = cheerio.load(html);
         // for each job item in the html
         $('.jobs-list .job-item').each(function(i, elem) {
-
-          var json_job = {
-                       id : "",
-                       title : "",
-                       company : "",
-                       url: "",
-                       localization: {
-                          data_workplace_name: "",
-                          data_workplace_lat: 0.0,
-                          data_workplace_lng: 0.0
-                       },
-                       contract :"",
-                       date : "",
-                       tags: []
-                     };
 
              //date formating if today (we do not want "il y a 3 heures")
              var item_date = $('.job-details-right', this).text();
@@ -85,37 +59,24 @@ function scrappHtml(html){
              // build job url base on rmx domain
              var item_url = remixUrl + $('.job-link', this).attr('href');
 
-            // json_job.id  = $(this).attr('data-job-id');
-            // json_job.title = item_title;
-            // json_job.url = item_url;
-            // json_job.company = $('.company', this).text();
-            // json_job.localization.data_workplace_name = $('.workplace', this).attr('data-workplace-name');
-            // json_job.localization.data_workplace_lat = $('.workplace', this).attr('data-workplace-lat');
-            // json_job.localization.data_workplace_lng = $('.workplace', this).attr('data-workplace-lng');
-            // json_job.contract = $('.contract', this).attr('data-contract-type');
-            // json_job.date = item_date;
-            // json_job.tags = item_tags;
+              var job = new Job();
+              job.id  = $(this).attr('data-job-id');
+              job.title = item_title;
+              job.url = item_url;
+              job.company = $('.company', this).text();
+              job.localization.data_workplace_name = $('.workplace', this).attr('data-workplace-name');
+              job.localization.data_workplace_lat = $('.workplace', this).attr('data-workplace-lat');
+              job.localization.data_workplace_lng = $('.workplace', this).attr('data-workplace-lng');
+              job.contract = $('.contract', this).attr('data-contract-type');
+              job.date = item_date;
+              job.tags = item_tags;
+              // save the bear and check for errors
+              job.save(function(err) {
+                  if (err)
+                      console.log(err);
 
+                  console.log("saving : " + job.id );
+              });
 
-                      var job = new Job();
-                      job.id  = $(this).attr('data-job-id');
-                      job.title = item_title;
-                      job.url = item_url;
-                      job.company = $('.company', this).text();
-                      job.localization.data_workplace_name = $('.workplace', this).attr('data-workplace-name');
-                      job.localization.data_workplace_lat = $('.workplace', this).attr('data-workplace-lat');
-                      job.localization.data_workplace_lng = $('.workplace', this).attr('data-workplace-lng');
-                      job.contract = $('.contract', this).attr('data-contract-type');
-                      job.date = item_date;
-                      job.tags = item_tags;
-                      // save the bear and check for errors
-                      job.save(function(err) {
-                          if (err)
-                              console.log(err);
-                      });
-
-
-        //  jsonResult.push(json_job);
         }); //end for each job_item in list
-        return "xx";
 }
--- server.js
+++ server.js
@@ -1,34 +1,19 @@
-var   express = require('express'),
-      path = require('path'),
-      bodyParser = require('body-parser'),
-      scrapper = require("./remixjob-scrapper"),
-      app = express();
-app.use(bodyParser.json()); // support json encoded bodies
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var Job        = require('./remixjob-model');
+var scrapper   = require("./remixjob-scrapper");
+// configure app to use bodyParser()
+// this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// custom parameters
-var number_of_pages_to_scrap = 5;
-
-/** ** ** ** ** ** **/
-/** API GET /POST   */
-/** ** ** ** ** ** **/
-
-
-app.get('/api/v1/jobs', function (req, res) {
-  res.write("list of jobs");
-  res.end();
-
-}); // end app.get
-
-app.get('/', function (req, res) {
-  res.write("aa");
-  res.end();
-
-}); // end app.get
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost/db-remixjob'); // connect to our database
 
 
 app.get('/scrap', function(req, res) {
-  var nbPages = number_of_pages_to_scrap;
+  var nbPages = 3;
   scrapper.scrappRemixJob(nbPages,function(hits){
     res.write("done. " + hits.toString() + " jobs found");
     res.end();
@@ -36,19 +21,54 @@ app.get('/scrap', function(req, res) {
 }); //end app.get /scrap
 
 
-
-// receiving the new bet
-app.post('/', function(req, res) {
-
-
-
- });
-
-
-
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Work in progress.');
+    next(); // make sure we go to the next routes and don't stop here
 });
+
+
+router.get('/', function(req, res) {
+    res.json({ message: 'welcome to the remix api!' });
+});
+
+router.route('/jobs')
+
+    // create a job (accessed at POST http://localhost:9292/api/jobs)
+    .post(function(req, res) {
+        // var job = new Job();      // create a new instance of the Bear model
+        // job.id= req.body.id;  // set the bears name (comes from the request)
+        //
+        // // save the bear and check for errors
+        // job.save(function(err) {
+        //     if (err)
+        //         res.send(err);
+        //
+        //     res.json({ message: 'Job created!' });
+        // });
+    })
+
+    // get all the jobs (accessed at GET http://localhost:9292/api/jobs)
+    .get(function(req, res) {
+        Job.find(function(err, jobs) {
+            if (err)
+                res.send(err);
+
+            res.json(jobs);
+        });
+    });
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api/v1', router);
+
+// START THE SERVER
+// =============================================================================
+
+var port = process.env.PORT || 9292;        // set our port
+app.listen(port);
+console.log('Magic happens on port ' + port);
